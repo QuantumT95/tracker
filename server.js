796e22b18f7c3a72529b23a7f00c5ce547b17ea5
QuantumT95 <tran.charlie@ymail.com>
2019-08-09 07:33:00 -0500

    removed yarn completely

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+//load config
+dotenv.config({ path: './config.env' });
+
+const app = express();
+
+//dev logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+//profile routes
+app.use('/api/v1/profile', require('./routes/profile'));
+
+//handle production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    //handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+const port = process.env.port || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${port}`);
+});
+
